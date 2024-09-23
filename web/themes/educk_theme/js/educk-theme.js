704b80ebb7994c820f023e068706226c92b74b31
miggarciacan@gmail.com <miggarciacan@gmail.com>
2024-09-22 19:14:23 -0500

    Creacion de theme general y modulo de user managment

---
+++ web/themes/educk_theme/js/educk-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * educk_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.educkTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
